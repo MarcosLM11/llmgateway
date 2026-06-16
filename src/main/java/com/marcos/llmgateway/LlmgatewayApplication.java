0fbb69c6ebdd3be659b1b20ea2b7258f16db1e56
Marcos <marcoslopezmarinn@gmail.com>
2026-06-16 14:30:59 +0200

    first commit

---
+++ src/main/java/com/marcos/llmgateway/LlmgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.llmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmgatewayApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(LlmgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/llmgateway/LlmgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.llmgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
